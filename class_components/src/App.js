724e1df96b1cdd201bc6afc0971cd19e26e227d2
kamy01 <andreikamy91@gmail.com>
2019-12-24 17:03:57 +0200

    strcture

--- class_components/src/App.js
+++ class_components/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header username="Andrei" />
+				<Greeting />
+			</div>
+		);
+	}
+}
+
+class Header extends React.Component {
+	render() {
+		return (
+			<header>
+				<p>Welcome, {this.props.username}!</p>
+			</header>
+		);
+	}
+}
+
+class Greeting extends React.Component {
+	getTimeOfDay() {
+		const date = new Date();
+		const hours = date.getHours();
+		let timeOfDay;
+
+		if (hours < 12) {
+			timeOfDay = "morning";
+		} else if (hours >= 12 && hours < 17) {
+			timeOfDay = "afternoon";
+		} else {
+			timeOfDay = "night";
+		}
+		return timeOfDay;
+	}
+
+	render() {
+		let timeOfDay = this.getTimeOfDay();
+		return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+	}
 }
 
 export default App;
