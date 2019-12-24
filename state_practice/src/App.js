0c31269396ae939bc962f216d6cc8f890e628c78
kamy01 <andreikamy91@gmail.com>
2019-12-24 17:58:58 +0200

    state change method

--- state_practice/src/App.js
+++ state_practice/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Challenge:
+// Given an incomplete class-based component without a constructor,
+// add a constructor and initialize state to fix the broken component.
+
+class App extends React.Component {
+	constructor() {
+    super()
+    this.state = {
+      name: "Andrei",
+      age: 28,
+      isLogged: false
+    }
+  }
+  loggStatus ()
+  {
+    const number = Math.random();
+    this.isLogged = number>0.5;
+    let status = this.isLogged  ? 'in' : 'out';
+    return status;
+  }
+	render() {
+		return (
+			<div>
+				<h1>{this.state.name}</h1>
+				<h3>{this.state.age} years old</h3>
+        <h1>You are currently logged  {this.loggStatus()}</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
