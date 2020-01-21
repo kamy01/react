b2cdd9161cfe7acc16c8b875845a12a2fa6fe700
kamy01 <andreikamy91@gmail.com>
2020-01-21 22:24:58 +0200

    change state

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+function handleOnChange() {
+	console.log('something changed');
+}
 function TodoItem(props) {
 	return (
 		<div className="todo-item">
-			<input type="checkbox" checked = {props.item.completed} />
+			<input type="checkbox" checked = {props.item.completed} onChange = {handleOnChange}/>
 			<p>{props.item.text}</p>
 		</div>
 	);
