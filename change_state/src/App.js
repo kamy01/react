8f842e407d5e60c38b9978fec9a29cc415878144
kamy01 <andreikamy91@gmail.com>
2020-01-21 22:41:20 +0200

    add decrement

--- change_state/src/App.js
+++ change_state/src/App.js
@@ -8,18 +8,28 @@ class App extends React.Component {
 		this.state = {
 			count: 0
 		};
-		this.increment = this.increment.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
 	}
 	increment() {
 		this.setState(prevState => {
 			return { count: prevState.count + 1 };
 		});
-	}
+  }
+  
+  decrement() {
+		this.setState(prevState => {
+			return { count: prevState.count - 1 };
+		});
+  }
+  
 	render() {
 		return (
 			<div>
 				<h1>{this.state.count}</h1>
-				<button onClick={this.increment}>Change!</button>
+				<button onClick={this.increment}>Increment!</button>
+        <br/>
+        <button onClick={this.decrement}>Decrement!</button>
 			</div>
 		);
 	}
