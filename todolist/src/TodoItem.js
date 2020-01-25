277c6c23a5c587794f39e2f8ed63cd9c022237ed
kamy01 <andreikamy91@gmail.com>
2020-01-25 18:05:27 +0200

    changing state with onChanged

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,11 +1,9 @@
 import React from "react";
-function handleOnChange() {
-	console.log('something changed');
-}
+
 function TodoItem(props) {
 	return (
 		<div className="todo-item">
-			<input type="checkbox" checked = {props.item.completed} onChange = {handleOnChange}/>
+			<input type="checkbox" checked = {props.item.completed} onChange = {() => props.handleChange(props.item.id)}/>
 			<p>{props.item.text}</p>
 		</div>
 	);
