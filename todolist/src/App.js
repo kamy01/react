2d91adf345df50e3eff0d3f80a09c6357a9e993c
kamy01 <andreikamy91@gmail.com>
2020-01-25 18:08:29 +0200

    format + small change

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -3,42 +3,42 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./data/todosData";
 
-import "./style.css"
-
+import "./style.css";
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            todos: todosData
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
-    
-    handleChange(id) {
-        console.log('Changed',id);
-        this.setState(prevState =>{
-            const updatedTodos = prevState.todos.map(todo => {
-                if(todo.id ===id){
-                    todo.completed= !todo.completed
-                }
-                return todo
-            })
-            return {
-                todos: updatedTodos
-            }
-        })
-    }
+	constructor() {
+		super();
+		this.state = {
+			todos: todosData
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-        
-        return (
-            <div className="todo-list">
-                {todoItems}
-            </div>
-        )    
-    }
+	handleChange(id) {
+		console.log("Changed", id);
+		this.setState(prevState => {
+			const updatedTodos = prevState.todos.map(todo => {
+				if (todo.id === id) {
+					return{
+                        ...todo,
+                        completed: !todo.completed
+                    }
+				}
+				return todo;
+			});
+			return {
+				todos: updatedTodos
+			};
+		});
+	}
+
+	render() {
+		const todoItems = this.state.todos.map(item => (
+			<TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+		));
+
+		return <div className="todo-list">{todoItems}</div>;
+	}
 }
 
-export default App
+export default App;
